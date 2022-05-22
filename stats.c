3583abbf7024936628eaec677863a4ffbbd804bb
Khushboo1107 <105203869+Khushboo1107@users.noreply.github.com>
2022-05-22 17:01:19 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,4 +1,5 @@
 #include "stats.h"
+#include "math.h"
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
@@ -10,11 +11,28 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
 float sum=0;
+float a;
 int setlength = sizeof(numberset) / sizeof(numberset[0]);
-int a=compute_statistics.setlength;
-for(int i = 0; i<=compute_statistics.setlength; i++)
+int l=compute_statistics.setlength;
+for(int i = 0; i<compute_statistics.setlength; i++)
 {
-    sum += scanf("(%f)", compute_statistics.numberset[]);
+    a = compute_statistics.numberset[i];
+    sum += compute_statistics.numberset[i];
+    if(a<s.min)
+    {
+        s.min=a;
+    }
+    if(a>s.max)
+    {
+        s.max=a;
+    }
 }
-s.average = sum/a;
-printf("(%f)",s.average);
+if(l==0)
+{
+    s.average=NAN;
+}
+else
+{
+    s.average = sum/l;
+    printf("(%f)",s.average);
+}

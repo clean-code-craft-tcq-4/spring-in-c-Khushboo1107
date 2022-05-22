8d5c99c4ecbca6068af9475921a2c792aa00d21b
Khushboo1107 <105203869+Khushboo1107@users.noreply.github.com>
2022-05-22 18:36:12 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -6,7 +6,7 @@ struct Stats {
   float average;
   float min;
   float max;
-}s;
+}stat;
 
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
